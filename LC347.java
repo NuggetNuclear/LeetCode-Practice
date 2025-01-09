0d323b9ca02f3012602e4b8f9adf950d4ea1e2f2
Gabriel <125526388+NuggetNuclear@users.noreply.github.com>
2025-01-09 15:11:44 -0300

    LeetCode 347 | Top K Frequent Elements

---
+++ LC347.java
@@ -0,0 +1,32 @@
+class LC347 {
+    public int[] topKFrequent(int[] nums, int k) {
+        int[] repetidos = new int[k];
+
+        HashMap<Integer, Integer> repetidosMapa = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            if(!repetidosMapa.containsKey(nums[i])) {
+                repetidosMapa.put(nums[i], 1);
+            } else {
+                repetidosMapa.put(nums[i], repetidosMapa.get(nums[i]) + 1);
+            }
+        }
+
+        for(int i = 0; i < k; i++) {
+            int indiceMayor = 0;
+
+            for(int j = 0; j < nums.length; j++) {
+                if(repetidosMapa.get(nums[indiceMayor]) < repetidosMapa.get(nums[j])) {
+                    indiceMayor = j;
+                }
+            }
+
+            int aux = nums[indiceMayor];
+            repetidos[i] = aux;
+
+            repetidosMapa.put(aux, 0);
+        }
+
+        return repetidos;
+    }
+}
